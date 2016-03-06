a5be373a93aa7f2386b0e037887908f4609eb6c7
Ling Kong <lingxkong@gmail.com>
2016-03-06 08:40:51 -0800

    fixed vertical alignments with line height

--- Twitch.tv API/script.js
+++ Twitch.tv API/script.js
@@ -1,11 +1,9 @@
-var userList = ["storbeck", "freecodecamp", "ESL_SC2", "OgamingSC2", "terakilobyte", "brunofin", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "comster404"];
+var userList = ["freecodecamp", "ESL_SC2", "OgamingSC2", "terakilobyte", "brunofin", "storbeck", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "comster404"];
 //var userEntry="";
-
 
 function loopJson(i) {
   var user = userList[i];
   $.getJSON("https://api.twitch.tv/kraken/streams/" + user + "?callback=?", function(data) {
-    
     var userStatus = "";
     var userGame = "";
     var userGameStatus = "";
@@ -13,8 +11,7 @@ function loopJson(i) {
     var userLogoURL = "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
     if (data.hasOwnProperty("stream")) {
       var userStream = data.stream;
-      if (userStream==null) 
-      {
+      if (userStream == null) {
         $.getJSON("https://api.twitch.tv/kraken/channels/" + user + "?callback=?", function(dataOff) {
           var userName = dataOff.display_name;
           var userStatus = "Offline";
@@ -24,12 +21,11 @@ function loopJson(i) {
           } else {
             var userLogoURL = "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
           }
-          var userEntry = "<div class=\"row\"><div class=\"user col-sm-2\"><a href=\"" + userURL + "\" target=\"_blank\"><img src=\"" + userLogoURL + "\" class=\"logo\" width=\"50\" height=\"50\"></a></div><div class=\"user col-md-3\"><a href=\"" + userURL + "\" target=\"_blank\">" + userName + "</a></div><div class=\"status col-md-7\">" + userStatus + "</div></div>";
+          var userEntry = "<div class=\"row entry\"><div class=\"user-logo col-sm-2\"><a href=\"" + userURL + "\" target=\"_blank\"><img src=\"" + userLogoURL + "\" class=\"logo\" width=\"50\" height=\"50\"></a></div><div class=\"user col-md-3\"><a href=\"" + userURL + "\" target=\"_blank\">" + userName + "</a></div><div class=\"status col-md-7\">" + userStatus + "</div></div>";
           var $entry = $("<p class='offline'>").html(userEntry);
-          $(".channel-list").append($entry);
+          $(".channel-offline").append($entry);
         });
-      }
-      else if (userStream.hasOwnProperty("channel")) {
+      } else if (userStream.hasOwnProperty("channel")) {
         var userName = userStream.channel.display_name;
         userStatus = "Online";
         userGame = userStream.game;
@@ -38,38 +34,20 @@ function loopJson(i) {
         if (userStream.channel.logo !== null) {
           userLogoURL = userStream.channel.logo;
         }
-        var userEntry = "<div class=\"row\"><div class=\"user col-sm-2\"><a href=\"" + userURL + "\" target=\"_blank\"><img src=\"" + userLogoURL + "\" class=\"logo\" width=\"50\" height=\"50\"></a></div><div class=\"user col-md-3\"><a href=\"" + userURL + "\" target=\"_blank\">" + userName + "</a></div><div class=\"status col-md-7\">" + userGame + ": " + userGameStatus + "</div></div>";
+        var userEntry = "<div class=\"row entry\"><div class=\"user-logo col-sm-2\"><a href=\"" + userURL + "\" target=\"_blank\"><img src=\"" + userLogoURL + "\" class=\"logo\" width=\"50\" height=\"50\"></a></div><div class=\"user col-md-3\"><a href=\"" + userURL + "\" target=\"_blank\">" + userName + "</a></div><div class=\"status col-md-7\">" + userGame + ": " + userGameStatus + "</div></div>";
         var $entry = $("<p class='online'>").html(userEntry);
-        $(".channel-list").append($entry);
-      } 
+        $(".channel-online").append($entry);
+      }
+    } else if (data.hasOwnProperty("error")) {
+      userStatus = "Account closed";
+      var userLogoURL = "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+      var userEntry = "<div class=\"row entry\"><div class=\"user-logo col-sm-2\"><img src=\"" + userLogoURL + "\" class=\"logo\" width=\"50\" height=\"50\"></div><div class=\"user col-md-3\">" + user + "</div><div class=\"status col-md-7\">" + userStatus + "</div></div>";
+      var $entry = $("<p class='offline'>").html(userEntry);
+      $(".channel-offline").append($entry);
     }
-    else if (data.hasOwnProperty("error")) {
-	    userStatus = "Account closed";
-	    var userLogoURL = "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
-	    var userEntry = "<div class=\"row\"><div class=\"user col-sm-2\"><img src=\"" + userLogoURL + "\" class=\"logo\" width=\"50\" height=\"50\"></div><div class=\"user col-md-3\">" + user + "</div><div class=\"status col-md-7\">" + userStatus + "</div></div>";
-	    var $entry = $("<p class='offline'>").html(userEntry);
-	    $(".channel-list").append($entry);
-    } 
   });
 
 }
-
-/*function onlineErrorUser(data) {
-  
-  
-  
-  
-  
-  else if ($userStream.hasOwnProperty("channel") === false) {
-      getJsonChannel($user);
-
-    }
-  } else if (data.hasOwnProperty("error")) {
-    $userStatus = "Account closed";
-    $userLogoURL = "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
-    $userEntry = $("<div>").html("<div class=\"user\"><img src=\"" + $userLogoURL + "\" class=\"logo\" width=\"50\" height=\"50\">" + $user + "</div><div class=\"status\">" + $userStatus + "</div>");
-  }
-}*/
 
 $(document).ready(
   function() {
