8bf95620356aab4af2d647b8eddd07966fad6c05
Ling Kong <lingxkong@gmail.com>
2016-03-17 11:28:42 -0700

    coded CE button (+3 squashed commit)
    Squashed commit:
    
    [a4b346c] coded AC button
    
    [b956b4a] Calculator field displays input from any button but does not distinguish between them yet.
    
    [482be1b] got the "7" button to place text into field.

--- JS Calculator/script.js
+++ JS Calculator/script.js
@@ -1,18 +1,60 @@
 var fieldText = "";
-
+var memory = "";
 function writeIn(btnText) {
 	var strText = btnText.toString();
 	fieldText+=strText;
-	console.log(fieldText);
+	memory=fieldText;
 	$(".field").text(fieldText);
 
 }
+/*
+function checkOperator(btnText){
+
+}
+*/ 
+function clear1(){
+
+	console.log(memory);
+	memory=memory.split("");
+
+	console.log(memory);
+	memory.pop();
+	console.log(memory);
+	memory=memory.join("");
+	fieldText=memory;
+	$(".field").text(fieldText);
+}
+
+function allClear(){
+	fieldText = "";
+	$(".field").text(fieldText);
+	memory = "";
+}
+
+/*
+function ans(){
+
+}
+
+function equals(str){
+
+}
+*/
 
 $(document).ready(
 	function() {
-		$("#n7").on("click", function(e){
-			var val = 7;
-			writeIn(val);
+		$("button").click(function(e){
+			var id = e.target.id; 
+			if (id==="AC"){
+				allClear();
+			}
+			else if (id==="CE") {
+				clear1();
+			}
+			else {
+				var val = $("#" + id).text();
+				writeIn(val);
+			}
 		});
 
 	}
