5762ea3ffd70b5eb12affa2cf6e9c6ffa60cb6ac
Ling Kong <lingxkong@gmail.com>
2016-03-24 15:55:27 -0700

    added some functions, still getting the framework in

--- Pomodoro Timer/script.js
+++ Pomodoro Timer/script.js
@@ -1,18 +1,36 @@
-function setTimerLength(){
-
+var function setTimerLength(){
+/*
+ * retrieve number of minutes from web page
+ * multiply by 60
+ * set to totalTimerSec
+ */	
+	runTimer(totalTimerSec);
 }
 
-function runTimer() {
+function setBreaklength(totalBreakSec) {
+	runTimer(totalBreakSec);
+}
 
+function timeToClock(time) {
+	var min = Math.floor(time/60);
+	var sec = Math.round(time % 60); 
+	var $time = min + " : " + sec;
+	$(".clock").text($time);
+}
+
+function countDown(totalTimerSec) {
+	totalTimerSec-=1;
+	timeToClock(totalTimerSec);
+}
+function runTimer() {
+var timer=setInterval(countDown(), 1000);
 }
 
 function stopTimer() {
-
+clearInterval(timer);
 }
 
-function setBreaklength() {
 
-}
 
 $(document).ready(function() {
 
